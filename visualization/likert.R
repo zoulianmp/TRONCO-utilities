1c58cd1c34f38e538f53a378d0851add0075ec6d
luca-dex <luca.desano@gmail.com>
2014-12-18 17:36:25 +0100

    stage-cluster graph (draft)

--- visualization/likert.R
+++ visualization/likert.R
@@ -16,16 +16,16 @@ require(devtools)
 #' @return The sum of \code{x} and \code{y}.
 likertToClus <- function(cluster_result, sample_stage, cluster_prefix='K', sample_prefix='S'){
   # check different value
-  cluster <- unique(cluster_result)
-  stage <- unique(sample_stage)
+  cluster <- sort(unique(cluster_result), decreasing = T)
+  stage <- sort(unique(sample_stage), na.last = T)
   
   # create label ['1', '2'] + prefix='k' -> ['K1', 'K2'] 
   cluster_label <- paste(cluster_prefix, cluster, sep='')
   stage_label <- paste(sample_prefix, stage, sep='')
   
   # create factor based on clustering and stage data
-  factor_k <- factor(cluster_result, labels = cluster_label)
-  factor_s <- factor(sample_stage, labels = stage_label)
+  factor_k <- factor(cluster_result, labels = cluster_label, exclude = NULL)
+  factor_s <- factor(sample_stage, labels = stage_label, exclude = NULL)
   
   # data frame c1->cluster, c2->stage
   df <- data.frame(factor_k, factor_s)
@@ -44,3 +44,27 @@ test_likertToClus <- function(K = 4, S = 6, sample = 250) {
   tab <- likertToClus(example_label, example_stage)
   plot(tab, centered = F, include.histogram = TRUE, col = terrain.colors(S))
 }
+
+
+# TODO: il blocco successivo va in una funzione
+
+
+sample_NBS_cluster <- read.csv('/home/dex/Dropbox/pyTSA/TCGA-CRC likert/samples-NBS-mapping.txt', header = TRUE, sep = ' ')
+sample_NBS_data <- read.csv('/home/dex/Dropbox/pyTSA/TCGA-CRC likert/tcga_infoClin_paper.txt', 
+                            header = TRUE, 
+                            sep = '\t', 
+                            row.names = 'patient')
+
+tumor_stage <-  subset(sample_NBS_data, select = 'tumor_stage')
+sample_NBS_cluster$old_name <- sample_NBS_cluster$Sample.ID
+sample_NBS_cluster$Sample.ID <- substr(sample_NBS_cluster$Sample.ID, 1, 12)
+sample_NBS_cluster <- sample_NBS_cluster[!duplicated(sample_NBS_cluster$Sample.ID), ]
+tumor_cluster = data.frame(sample_NBS_cluster$Cluster, sample_NBS_cluster$old_name, row.names=sample_NBS_cluster$Sample.ID)
+sample_stage_cluster <- merge(tumor_stage, tumor_cluster, by = 0, all=T)
+
+#levels(sample_stage_cluster$tumor_stage)
+levels(sample_stage_cluster$sample_NBS_cluster.Cluster) <- sort(unique(sample_stage_cluster$sample_NBS_cluster.Cluster))
+#levels(sample_stage_cluster$sample_NBS_cluster.Cluster)
+
+result <- likertToClus(sample_stage_cluster$sample_NBS_cluster.Cluster, sample_stage_cluster$tumor_stage, sample_prefix = '', cluster_prefix ='')
+plot(result, centered=F, include.histogram = TRUE, col = brewer.pal(length(unique(sample_stage_cluster$tumor_stage)), "YlOrRd"))
